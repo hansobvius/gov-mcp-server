baa8a90d3dfb0c9897eb102bd6f33d431d64f75a
Thiago Caiado <thiago.caiado@hotmail.com>
2025-10-26 13:48:26 -0300

    adjustment on ndeputy consult for search by name and query deputy details by id

--- src/api/deputados/deputados_api.py
+++ src/api/deputados/deputados_api.py
@@ -4,11 +4,35 @@ from typing import Any, Dict
 from src.config import URL_BASE_API
 
 
-async def get_deputados(name: str) -> Dict[str, Any]:
+async def get_deputados_by_name(name: str) -> Dict[str, Any]:
     async with httpx.AsyncClient() as client:
         try:
+            params = {
+                "nome": name,
+                "ordem": "ASC",
+                "ordernarPor": "nome"
+            }
+            response = await client.get(
+                f"{URL_BASE_API}/deputados",
+                params=params,
+                timeout=30.0)
+            response.raise_for_status()
+            return response.json()
+        except httpx.RequestError as exc:
+            return {
+                "status": "Error",
+                "message": exc
+            }
+
+async def get_deputados_details(id: int) -> Dict[str, any]:
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.get(f"{URL_BASE_API}/deputados/{id}", timeout=30.0)
             response = await client.get(URL_BASE_API, timeout=30.0)
             response.raise_for_status()
             return response.json()
-        except Exception:
-            return None
+        except httpx.RequestError as exc:
+            return {
+                "status": "Error",
+                "message": exc
+            }
